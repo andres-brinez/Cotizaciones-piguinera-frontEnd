9632c3d07756d5068a0493cf8c1f2b058c46bbc2
Andres Briñez <brinezlopez08@gmail.com>
2024-04-18 12:07:21 -0500

    Estructura básica del proyecto

---
+++ project/src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+      {/* <SectionHeader /> */}
+      <main>
+      </main>
+      {/* <SectionFooter/> */}
+    </>,
+)
