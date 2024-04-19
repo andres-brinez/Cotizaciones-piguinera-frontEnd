24d74af5794eb634b685ccd442208912ad6d5f95
Andres Briñez <brinezlopez08@gmail.com>
2024-04-19 00:01:49 -0500

    Se agrega la funcionalidad de logout

--- project/src/components/NavigationBar/index.tsx
+++ project/src/components/NavigationBar/index.tsx
@@ -1,23 +1,42 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate, } from 'react-router-dom';
+import { AppContext } from "../../state/AppContext";
+import { StorageService } from "../../core/services/general/storage.service";
 
 export function NavigationBar() {
+
+    const { dispatch } = useContext(AppContext);
+    const navigateTo = useNavigate();
+    const storage = new StorageService();
+
+    const handleLogout = () => {
+
+        const isLogout = window.confirm('¿Estás seguro que deseas cerrar la sesión?');
+        if (isLogout) {
+            dispatch({ type: 'USER_LOGGED_OUT' });
+            storage.set('TOKEN', '');
+            navigateTo('/login');
+        }
+
+    };
+
     return (
         <nav className='header__nav'>
             <ul>
                 <li>
-                <Link className='header__link' to='/budget'>Presupuesto</Link>
+                    <Link className='header__link' to='/budget'>Presupuesto</Link>
                 </li>
                 <li>
-                <Link className='header__link' to='/quotes'>Cotizacion</Link>
+                    <Link className='header__link' to='/quotes'>Cotizacion</Link>
                 </li>
                 <li>
-                <Link className='header__link' to='/addBook'>Add Book</Link> 
+                    <Link className='header__link' to='/addBook'>Add Book</Link>
                 </li>
                 <li>
                     <Link className='header__link' to='/home'>Home</Link>
                 </li>
                 <li>
-                    <Link className='header__link' to='/login'>Salir</Link>
+                    <a onClick={handleLogout}>Cerrar sesión</a>
                 </li>
             </ul>
         </nav>
