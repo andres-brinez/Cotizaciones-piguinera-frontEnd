e8aac2ec4c9a998066874eb9134aeb06eeccc8dd
Andres Briñez <brinezlopez08@gmail.com>
2024-04-18 22:28:40 -0500

    Add getBooksService function to books.service.ts

---
+++ project/src/core/services/books.service.ts
@@ -0,0 +1,35 @@
+import { IBookModel } from "../../models/book.model";
+import { urls } from "../env/url.resource";
+import http from "./general/http.service";
+
+
+export const getBooksService = (): Promise<IBookModel[]> => {
+  const url = urls.getBooks;
+  return http.get(url)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        alert('no estas autorizado')
+      }
+    })
+    .then((data) => {
+      console.log(data.books)
+      if (Array.isArray(data.books)) {
+        return data.books.map((book: any) => {
+          return {
+            Id: book.id,
+            Title: book.title,
+            EmailProvider: book.emailProvider,
+            OriginalPrice: book.originalPrice,
+            Quantity: book.quantity,
+            Type: book.type,
+            UnitPrice: book.unitPrice,
+            Discount: book.discount
+          }
+        });
+      } else {
+        throw new Error('La respuesta de la API no es un array');
+      }
+    });
+}
