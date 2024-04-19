51015f34d372f99f251f6303145e9e2b77163d81
Andres Briñez <brinezlopez08@gmail.com>
2024-04-19 00:31:11 -0500

    Fix authentication bug and decode JWT token in useAuthLogin hook

--- project/src/hooks/useAuth.ts
+++ project/src/hooks/useAuth.ts
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { authServiceLogin } from "../core/services/auth.service";
 import { useContext, useState } from "react";
 import { AppContext } from "../state/AppContext";
+import { jwtDecode } from "jwt-decode";
 
 export const useAuthLogin = () => {
 
@@ -10,10 +11,15 @@ export const useAuthLogin = () => {
   const {dispatch} = useContext(AppContext);
 
   const authenticate = (email: string, password: string) => authServiceLogin({ Email: email, password: password }).then((response) => {
-    if (response) {
-      dispatch({ type: 'USER_LOGGED' });
-      navigateTo('/home');
+    
+    const token = response;
+    
+    if (token) {
+      const decodedToken = jwtDecode(token) as { [key: string]: any };
+      const email = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/userdata'];      
+      dispatch({ type: 'USER_LOGGED', payload: email });
 
+      navigateTo('/home');
     } else {
       setErrorMessage('Las credenciales proporcionadas son incorrectas.');
     }
