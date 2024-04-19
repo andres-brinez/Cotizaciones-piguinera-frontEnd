401490bd525ffc656f1dfc93aeda0a10d806c07f
Andres Briñez <brinezlopez08@gmail.com>
2024-04-18 22:28:10 -0500

    Add login functionality to Login component

--- project/src/components/Login/index.tsx
+++ project/src/components/Login/index.tsx
@@ -1,27 +1,46 @@
 import React, { useState } from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { authServiceLogin } from '../../core/services/auth.service';
 // import { Header } from '../Header';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const navigateTo = useNavigate();
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    // Aquí se envia la información a la API
-    alert(`¡Hola ${email}! estas logueado!`);
-  };
+    setErrorMessage('');
+
+    authServiceLogin({ Email: email, password: password }).then((response) => {
+      if (response) {
+        navigateTo('/home');
+
+      } else {
+        setErrorMessage('Las credenciales proporcionadas son incorrectas.');
+      } 
+    })
+  }
 
   return (
     <>
       <h2 className='subtitle'> Loguear</h2>
       <form className="login" onSubmit={handleSubmit}>
+        <fieldset>
+        <legend className="login__legend">Iniciar sesión ingresa tus credenciales</legend>
         <label className="login__label" htmlFor="email">Correo electrónico:</label>
-        <input className="login__input" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input className="login__input" type="email" name='userEmail' id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
         <label className="login__label" htmlFor="password">Contraseña:</label>
-        <input className="login__input" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <input className="login__input" type="password" name='userPassword' id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+
+        </fieldset>
+
+        {/* si el erro existe */}
+        {errorMessage && <p className="login__error">{errorMessage}</p>}
 
         <button className="login__button" type="submit">Iniciar sesión</button>
       </form>
