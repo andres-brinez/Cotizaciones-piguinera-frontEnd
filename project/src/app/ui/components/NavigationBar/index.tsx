b5dde81821e4d3fe5824fc6bdc7f415da7595d51
Andres Briñez <brinezlopez08@gmail.com>
2024-04-19 21:53:37 -0500

    Se hacen cmbios en las rutas

--- project/src/app/ui/components/NavigationBar/index.tsx
+++ project/src/app/ui/components/NavigationBar/index.tsx
@@ -1,44 +1,57 @@
 import { useLogout } from '../../../core/hooks/useLogout';
 import { IItemMenu } from '../../../core/models/item-menu.model';
 import { ItemMenu } from '../../elements/ItemMenu';
+import img from '../../../../assets/Pingüinera .png';
+import { useLocation } from 'react-router-dom';
 
 import './style.css';
+import { ReactSVG } from 'react-svg';
 
 export function NavigationBar() {
 
     const logout = useLogout();
+    const location = useLocation();
 
-    
+
     const menuItems: IItemMenu[] = [
         {
             text: 'Presupuesto',
-            url: '/budget'
+            url: '/budget',
+            imgSrc: '/src/assets/budget.svg'
+
         },
         {
             text: 'Cotizacion',
-            url: '/quotes'
+            url: '/quotes',
+            imgSrc: '/src/assets/quotes.svg'
+
         },
-        
+
         {
             text: 'Home',
-            url: '/home'
+            url: '/home',
+            imgSrc: 'src/assets/Home.svg'
+
         }
     ];
 
     return (
         <nav className='navbar'>
-            <h2 className='navbar__header'>La Pinguinera</h2>
+            <img src={img} alt="Logo de la pinguinera" className='navbar__logo' />
             <ul className='navbar__menu'>
                 {
                     menuItems.map((item) => (
-                        <li className='navbar__item'>
-                            <ItemMenu  key={item.text} text={item.text} url={item.url} />
+                        <li className={`navbar__item ${location.pathname === item.url ? 'navbar__item--active' : ''}`} key={item.text}>
+                        {item.imgSrc && <ReactSVG src={item.imgSrc} className="navbar__item-img" />}
+                            <ItemMenu text={item.text} url={item.url} />
                         </li>
                     ))
                 }
-                <li className='navbar__item'>
-                <a className='navbar__link' onClick={logout}>Cerrar sesión</a>                </li>
+
             </ul>
+            <footer className='navbar__logout'>
+                <a className='navbar__link' onClick={logout}>Cerrar sesión</a>
+            </footer>
         </nav>
     );
 }
