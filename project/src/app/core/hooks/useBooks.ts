4bc2aabf3b6d64cb1a3710747d11a0a76f2bbb39
Andres Briñez <brinezlopez08@gmail.com>
2024-05-24 00:40:38 -0500

    Cambios en el reducer

--- project/src/app/core/hooks/useBooks.ts
+++ project/src/app/core/hooks/useBooks.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { getBooksService } from "../services/books.service";
 import { useBookReducer } from "./useBooksReducer";
+import { IBookModel } from "../models/book.model";
 
 export const useBooks = () => {
   const [isLoading, setIsLoading] = useState(false);
   const {_updateBooks} = useBookReducer();
-
+  const [books, setBooks] = useState<IBookModel[]>([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -13,11 +14,12 @@ export const useBooks = () => {
       console.log('response desde el homepage')
       console.log(response)
       if (response) {
+        setBooks(response);
         _updateBooks(response);
       }
       setIsLoading(false);
     });
   }, []);
-  return {isLoading }
+  return {isLoading,books }
 }
 
--- project/src/app/core/store/reducers/book.reduce.ts
+++ project/src/app/core/store/reducers/book.reduce.ts
@@ -1,6 +1,7 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { IBookModel } from "../../models/book.model";
 import { RootState } from "../store";
+import { useBooks } from "../../hooks/useBooks";
 
 export interface Books {
   books: IBookModel[];
@@ -10,18 +11,31 @@ const initialState: Books = {
   books: [],
 }
 
+export const getBooks = createAsyncThunk(
+  "books/getBooks",
+  async () => {
+    const { books } = useBooks();
+    return books;
+  }
+);
+
 // Definición del reducer
 export const userSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     // recibe un PayloadAction<string>, lo que significa que el payload debe ser una cadena (en este caso, el email del usuario). Luego, actualizamos la propiedad userName del estado con el valor del payload.
-    updateBooks: (state, action: PayloadAction< IBookModel[]>) => {
+    updateBooks: (state, action: PayloadAction<IBookModel[]>) => {
       state.books = action.payload;
     },
-
   },
-  
+  extraReducers: builder => {
+    builder
+      .addCase(getBooks.fulfilled, (state, action) => {
+        state.books = action.payload;
+      })
+  }
+
 })
 
 
